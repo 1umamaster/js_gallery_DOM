393d68d4d0307fa43a648d2cd7891556db027b93
Oleh Lukash <lukasholeg02@gmail.com>
2024-12-15 00:15:13 +0300

    make build

---
+++ index.5cb3c9f0.js
@@ -0,0 +1,2 @@
+var e,t;e=document.querySelector("#thumbs"),t=document.querySelector("#largeImg"),e.addEventListener("click",function(e){e.preventDefault();var r=e.target.closest("a").getAttribute("href");t.setAttribute("src",r)});
+//# sourceMappingURL=index.5cb3c9f0.js.map
